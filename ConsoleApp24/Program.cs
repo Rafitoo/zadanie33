55fdf2946b694a275b14aec5f6a07e1b15a064c2
Rafitoo <137788085+Rafitoo@users.noreply.github.com>
2023-06-26 16:55:59 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Wektor v1 = new Wektor(1, 2, 3);
+        Wektor v2 = new Wektor(4, 5, 6);
+        Wektor suma = v1 + v2;
+        Wektor różnica = v2 - v1;
+        Wektor iloczyn = v1 * 2.5;
+        Wektor iloraz = v2 / 3;
+
+        Console.WriteLine("Wektor v1: " + WypiszWspółrzędne(v1));
+        Console.WriteLine("Wektor v2: " + WypiszWspółrzędne(v2));
+        Console.WriteLine("Suma v1 + v2: " + WypiszWspółrzędne(suma));
+        Console.WriteLine("Różnica v2 - v1: " + WypiszWspółrzędne(różnica));
+        Console.WriteLine("Iloczyn v1 * 2.5: " + WypiszWspółrzędne(iloczyn));
+        Console.WriteLine("Iloraz v2 / 3: " + WypiszWspółrzędne(iloraz));
+
+        double iloczynSkalarny = Wektor.IloczynSkalarny(v1, v2);
+        Console.WriteLine("Iloczyn skalarny v1 * v2: " + iloczynSkalarny);
+
+        Console.ReadKey();
+    }
+
+    private static string WypiszWspółrzędne(Wektor v)
+    {
+        string współrzędne = "";
+        for (byte i = 0; i < v.Wymiar; i++)
+        {
+            współrzędne += v[i] + " ";
+        }
+        return współrzędne;
+    }
+}
+
---
+++ ConsoleApp24/Wektor.cs
@@ -0,0 +1,107 @@
+﻿
+public class Wektor
+{
+    private double[] współrzędne;
+
+    public Wektor(params double[] współrzędne)
+    {
+        this.współrzędne = (double[])współrzędne.Clone();// klonować .clone utworzyć nową tablicę skopiować rzeczy je tam rzucić - czyli stworzyc nową tablicę żeby nie psuło hermetyzacji , z metoda array clone , i użyć castnig , chodzi o to żeby nik nie mógł pozmieniać zawartości       
+    }
+    
+    public byte Wymiar => (byte)współrzędne.Length;
+
+    public double this[byte indeks] 
+    {
+        get { return współrzędne[indeks]; }
+        set { współrzędne[indeks] = value; }
+    }
+
+    public double Długość
+    {
+        get { return Math.Sqrt(IloczynSkalarny(this, this)); }
+    }
+
+    public static double IloczynSkalarny(Wektor V, Wektor W)
+    {
+        if (V.Wymiar != W.Wymiar)
+        {
+            throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+        }
+
+        double iloczyn = 0;
+        for (byte i = 0; i < V.Wymiar; i++)
+        {
+            iloczyn += V[i] * W[i];
+        }
+        return iloczyn;
+    }
+
+    public static Wektor Suma(params Wektor[] Wektory)
+    {
+        if (Wektory.Length == 0)
+        {
+            throw new ArgumentException("Brak wektorów do zsumowania.");
+        }
+
+        byte wymiar = Wektory[0].Wymiar;
+        for (int i = 1; i < Wektory.Length; i++)
+        {
+            if (Wektory[i].Wymiar != wymiar)
+            {
+                throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+            }
+        }
+
+        double[] noweWspółrzędne = new double[wymiar];
+        for (byte i = 0; i < wymiar; i++)
+        {
+            double suma = 0;
+            for (int j = 0; j < Wektory.Length; j++)
+            {
+                suma += Wektory[j][i];
+            }
+            noweWspółrzędne[i] = suma;
+        }
+        return new Wektor(noweWspółrzędne);
+    }
+
+    public static Wektor operator +(Wektor V, Wektor W)
+    {
+        return Suma(V, W);
+    }
+
+    public static Wektor operator -(Wektor V, Wektor W)
+    {
+        return Suma(V, -1 * W);
+    }
+
+    public static Wektor operator *(Wektor v, double skalar)
+    {
+        double[] noweWspółrzędne = new double[v.Wymiar];
+        for (byte i = 0; i < v.Wymiar; i++)
+        {
+            noweWspółrzędne[i] = v[i] * skalar;
+        }
+        return new Wektor(noweWspółrzędne);
+    }
+
+    public static Wektor operator *(double skalar, Wektor v)
+    {
+        return v * skalar;
+    }
+
+    public static Wektor operator /(Wektor v, double skalar)
+    {
+        if (skalar == 0)
+        {
+            throw new DivideByZeroException("Nie można dzielić przez zero.");
+        }
+
+        double[] noweWspółrzędne = new double[v.Wymiar];
+        for (byte i = 0; i < v.Wymiar; i++)
+        {
+            noweWspółrzędne[i] = v[i] / skalar;
+        }
+        return new Wektor(noweWspółrzędne);
+    }
+}
